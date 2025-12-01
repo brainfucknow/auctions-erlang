1c9e3dcdf69e35f1e4058601dabd9c25626eed81
Mindu Gamese <mindgamese@gmail.com>
2025-12-01 18:56:54 +0000

    init

---
+++ src/openapi_create_auction_handler.erl
@@ -0,0 +1,115 @@
+-module(openapi_create_auction_handler).
+-moduledoc """
+Exposes the following operation IDs:
+
+- `POST` to `/auctions`, OperationId: `create_auction`:
+Create an auction.
+Create an auction. Note that the auction models are restricted to be a closed bid auction either as a [First price sealed bid auction](https://en.wikipedia.org/wiki/First-price_sealed-bid_auction) or a [Vickrey auction](https://en.wikipedia.org/wiki/Vickrey_auction). It can also be a [Timed ascending auction also known as an English auction](https://en.wikipedia.org/wiki/English_auction).
+
+""".
+
+-behaviour(cowboy_rest).
+
+-include_lib("kernel/include/logger.hrl").
+
+%% Cowboy REST callbacks
+-export([init/2]).
+-export([allowed_methods/2]).
+-export([content_types_accepted/2]).
+-export([content_types_provided/2]).
+-export([delete_resource/2]).
+-export([is_authorized/2]).
+-export([valid_content_headers/2]).
+-export([handle_type_accepted/2, handle_type_provided/2]).
+
+-ignore_xref([handle_type_accepted/2, handle_type_provided/2]).
+
+-export_type([class/0, operation_id/0]).
+
+-type class() :: 'createAuction'.
+
+-type operation_id() ::
+    'create_auction'. %% Create an auction
+
+
+-record(state,
+        {operation_id :: operation_id(),
+         accept_callback :: openapi_logic_handler:accept_callback(),
+         provide_callback :: openapi_logic_handler:provide_callback(),
+         api_key_callback :: openapi_logic_handler:api_key_callback(),
+         context = #{} :: openapi_logic_handler:context()}).
+
+-type state() :: #state{}.
+
+-spec init(cowboy_req:req(), openapi_router:init_opts()) ->
+    {cowboy_rest, cowboy_req:req(), state()}.
+init(Req, {Operations, Module}) ->
+    Method = cowboy_req:method(Req),
+    OperationID = maps:get(Method, Operations, undefined),
+    ?LOG_INFO(#{what => "Attempt to process operation",
+                method => Method,
+                operation_id => OperationID}),
+    State = #state{operation_id = OperationID,
+                   accept_callback = fun Module:accept_callback/4,
+                   provide_callback = fun Module:provide_callback/4,
+                   api_key_callback = fun Module:api_key_callback/2},
+    {cowboy_rest, Req, State}.
+
+-spec allowed_methods(cowboy_req:req(), state()) ->
+    {[binary()], cowboy_req:req(), state()}.
+allowed_methods(Req, #state{operation_id = 'create_auction'} = State) ->
+    {[<<"POST">>], Req, State};
+allowed_methods(Req, State) ->
+    {[], Req, State}.
+
+-spec is_authorized(cowboy_req:req(), state()) ->
+    {true | {false, iodata()}, cowboy_req:req(), state()}.
+is_authorized(Req, State) ->
+    {true, Req, State}.
+
+-spec content_types_accepted(cowboy_req:req(), state()) ->
+    {[{binary(), atom()}], cowboy_req:req(), state()}.
+content_types_accepted(Req, #state{operation_id = 'create_auction'} = State) ->
+    {[
+      {<<"application/json">>, handle_type_accepted},
+      {<<"text/json">>, handle_type_accepted},
+      {<<"application/*+json">>, handle_type_accepted}
+     ], Req, State};
+content_types_accepted(Req, State) ->
+    {[], Req, State}.
+
+-spec valid_content_headers(cowboy_req:req(), state()) ->
+    {boolean(), cowboy_req:req(), state()}.
+valid_content_headers(Req, #state{operation_id = 'create_auction'} = State) ->
+    {true, Req, State};
+valid_content_headers(Req, State) ->
+    {false, Req, State}.
+
+-spec content_types_provided(cowboy_req:req(), state()) ->
+    {[{binary(), atom()}], cowboy_req:req(), state()}.
+content_types_provided(Req, #state{operation_id = 'create_auction'} = State) ->
+    {[], Req, State};
+content_types_provided(Req, State) ->
+    {[], Req, State}.
+
+-spec delete_resource(cowboy_req:req(), state()) ->
+    {boolean(), cowboy_req:req(), state()}.
+delete_resource(Req, State) ->
+    {Res, Req1, State1} = handle_type_accepted(Req, State),
+    {true =:= Res, Req1, State1}.
+
+-spec handle_type_accepted(cowboy_req:req(), state()) ->
+    { openapi_logic_handler:accept_callback_return(), cowboy_req:req(), state()}.
+handle_type_accepted(Req, #state{operation_id = OperationID,
+                                 accept_callback = Handler,
+                                 context = Context} = State) ->
+    {Res, Req1, Context1} = Handler(createAuction, OperationID, Req, Context),
+    {Res, Req1, State#state{context = Context1}}.
+
+-spec handle_type_provided(cowboy_req:req(), state()) ->
+    { openapi_logic_handler:provide_callback_return(), cowboy_req:req(), state()}.
+handle_type_provided(Req, #state{operation_id = OperationID,
+                                 provide_callback = Handler,
+                                 context = Context} = State) ->
+    {Res, Req1, Context1} = Handler(createAuction, OperationID, Req, Context),
+    {Res, Req1, State#state{context = Context1}}.
---
+++ src/openapi_router.erl
@@ -0,0 +1,78 @@
+-module(openapi_router).
+
+-export([get_paths/1]).
+
+-type method() :: binary().
+-type operations() :: #{method() => openapi_api:operation_id()}.
+-type init_opts()  :: {operations(), module()}.
+
+-export_type([init_opts/0]).
+
+-spec get_paths(LogicHandler :: module()) -> cowboy_router:routes().
+get_paths(LogicHandler) ->
+    PreparedPaths = maps:fold(
+                      fun(Path, #{operations := Operations, handler := Handler}, Acc) ->
+                              [{Path, Handler, Operations} | Acc]
+                      end, [], group_paths()
+                     ),
+    [{'_', [{P, H, {O, LogicHandler}} || {P, H, O} <- PreparedPaths]}].
+
+group_paths() ->
+    maps:fold(
+      fun(OperationID, #{servers := Servers, base_path := BasePath, path := Path,
+                         method := Method, handler := Handler}, Acc) ->
+              FullPaths = build_full_paths(Servers, BasePath, Path),
+              merge_paths(FullPaths, OperationID, Method, Handler, Acc)
+      end, #{}, get_operations()).
+
+build_full_paths([], BasePath, Path) ->
+    [lists:append([BasePath, Path])];
+build_full_paths(Servers, _BasePath, Path) ->
+    [lists:append([Server, Path]) || Server <- Servers ].
+
+merge_paths(FullPaths, OperationID, Method, Handler, Acc) ->
+    lists:foldl(
+      fun(Path, Acc0) ->
+              case maps:find(Path, Acc0) of
+                  {ok, PathInfo0 = #{operations := Operations0}} ->
+                      Operations = Operations0#{Method => OperationID},
+                      PathInfo = PathInfo0#{operations => Operations},
+                      Acc0#{Path => PathInfo};
+                  error ->
+                      Operations = #{Method => OperationID},
+                      PathInfo = #{handler => Handler, operations => Operations},
+                      Acc0#{Path => PathInfo}
+              end
+      end, Acc, FullPaths).
+
+get_operations() ->
+    #{ 
+       'create_auction' => #{
+            servers => [],
+            base_path => "",
+            path => "/auctions",
+            method => <<"POST">>,
+            handler => 'openapi_create_auction_handler'
+        },
+       'add_bid' => #{
+            servers => [],
+            base_path => "",
+            path => "/auctions/:auctionId/bids",
+            method => <<"POST">>,
+            handler => 'openapi_create_bid_handler'
+        },
+       'get_auction' => #{
+            servers => [],
+            base_path => "",
+            path => "/auctions/:auctionId",
+            method => <<"GET">>,
+            handler => 'openapi_get_auctions_handler'
+        },
+       'get_auctions' => #{
+            servers => [],
+            base_path => "",
+            path => "/auctions",
+            method => <<"GET">>,
+            handler => 'openapi_get_auctions_handler'
+        }
+    }.
