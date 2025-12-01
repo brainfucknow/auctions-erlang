b762c1648f34ead7f7511669a0100969f8c4bbb0
Mindu Gamese <mindgamese@gmail.com>
2025-12-01 19:50:16 +0000

    serialization

---
+++ src/auction_serialization.erl
@@ -0,0 +1,137 @@
+-module(auction_serialization).
+-export([decode/1, encode/1, read_jsonl/1]).
+-export([decode_auction_type/1, encode_auction_type/1]).
+-export([decode_user/1, encode_user/1]).
+-export([parse_amount/1, format_amount/1]).
+
+decode(Json) when is_binary(Json) ->
+    try
+        Map = json:decode(Json),
+        decode_map(Map)
+    catch
+        _:_ -> {error, invalid_json}
+    end.
+
+decode_map(#{<<"$type">> := <<"AddAuction">>} = Map) ->
+    #{<<"at">> := At, <<"auction">> := AuctionMap} = Map,
+    {add_auction, At, decode_auction(AuctionMap)};
+decode_map(#{<<"$type">> := <<"PlaceBid">>} = Map) ->
+    #{<<"at">> := At, <<"bid">> := BidMap} = Map,
+    {place_bid, At, decode_bid(BidMap)};
+decode_map(#{<<"$type">> := Type}) ->
+    {error, {unknown_type, Type}};
+decode_map(_) ->
+    {error, missing_type_field}.
+
+decode_auction(Map) ->
+    #{<<"id">> := Id,
+      <<"startsAt">> := StartsAt,
+      <<"title">> := Title,
+      <<"expiry">> := Expiry,
+      <<"user">> := UserStr,
+      <<"type">> := TypeStr,
+      <<"currency">> := Currency} = Map,
+    #{id => Id,
+      starts_at => StartsAt,
+      title => Title,
+      expiry => Expiry,
+      user => decode_user(UserStr),
+      type => decode_auction_type(TypeStr),
+      currency => Currency}.
+
+decode_bid(Map) ->
+    #{<<"id">> := Id,
+      <<"auction">> := AuctionId,
+      <<"user">> := UserStr,
+      <<"amount">> := Amount,
+      <<"at">> := At} = Map,
+    #{id => Id,
+      auction_id => AuctionId,
+      user => decode_user(UserStr),
+      amount => Amount,
+      at => At}.
+
+decode_user(Bin) ->
+    case binary:split(Bin, <<"|">>, [global]) of
+        [<<"BuyerOrSeller">>, Id, Name] -> #{id => Id, name => Name};
+        _ -> Bin
+    end.
+
+decode_auction_type(Bin) ->
+    case binary:split(Bin, <<"|">>, [global]) of
+        [<<"English">>, Min, Reserve, Inc] ->
+            {english, binary_to_integer(Min), binary_to_integer(Reserve), binary_to_integer(Inc)};
+        [<<"Vickrey">>] -> vickrey;
+        [<<"Blind">>] -> blind;
+        _ -> Bin
+    end.
+
+encode({add_auction, At, Auction}) ->
+    Map = #{<<"$type">> => <<"AddAuction">>,
+            <<"at">> => At,
+            <<"auction">> => encode_auction(Auction)},
+    json:encode(Map);
+encode({place_bid, At, Bid}) ->
+    Map = #{<<"$type">> => <<"PlaceBid">>,
+            <<"at">> => At,
+            <<"bid">> => encode_bid(Bid)},
+    json:encode(Map).
+
+encode_auction(Auction) ->
+    #{id := Id,
+      starts_at := StartsAt,
+      title := Title,
+      expiry := Expiry,
+      user := User,
+      type := Type,
+      currency := Currency} = Auction,
+    #{<<"id">> => Id,
+      <<"startsAt">> => StartsAt,
+      <<"title">> => Title,
+      <<"expiry">> => Expiry,
+      <<"user">> => encode_user(User),
+      <<"type">> => encode_auction_type(Type),
+      <<"currency">> => Currency}.
+
+encode_bid(Bid) ->
+    #{id := Id,
+      auction_id := AuctionId,
+      user := User,
+      amount := Amount,
+      at := At} = Bid,
+    #{<<"id">> => Id,
+      <<"auction">> => AuctionId,
+      <<"user">> => encode_user(User),
+      <<"amount">> => Amount,
+      <<"at">> => At}.
+
+encode_user(#{id := Id, name := Name}) ->
+    iolist_to_binary([<<"BuyerOrSeller|">>, Id, <<"|">>, Name]);
+encode_user(Bin) when is_binary(Bin) -> Bin.
+
+encode_auction_type({english, Min, Reserve, Inc}) ->
+    iolist_to_binary([<<"English|">>, integer_to_integer(Min), <<"|">>, integer_to_integer(Reserve), <<"|">>, integer_to_integer(Inc)]);
+encode_auction_type(vickrey) -> <<"Vickrey">>;
+encode_auction_type(blind) -> <<"Blind">>.
+
+integer_to_integer(I) when is_integer(I) -> integer_to_binary(I);
+integer_to_integer(B) when is_binary(B) -> B.
+
+read_jsonl(Filename) ->
+    case file:read_file(Filename) of
+        {ok, Binary} ->
+            Lines = binary:split(Binary, <<"\n">>, [global, trim]),
+            {ok, [decode(Line) || Line <- Lines, Line =/= <<>>]};
+        Error -> Error
+    end.
+
+parse_amount(Bin) ->
+    case re:run(Bin, "^([A-Z]+)([0-9]+)$", [{capture, all_but_first, binary}]) of
+        {match, [Currency, AmountStr]} ->
+            {ok, {amount, Currency, binary_to_integer(AmountStr)}};
+        nomatch ->
+            {error, invalid_amount_format}
+    end.
+
+format_amount({amount, Currency, Value}) ->
+    iolist_to_binary([Currency, integer_to_binary(Value)]).
---
+++ test/serialization_tests.erl
@@ -0,0 +1,64 @@
+-module(serialization_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+read_commands_test() ->
+    {ok, Cmds} = auction_serialization:read_jsonl("test/samples/sample-commands.jsonl"),
+    ?assert(length(Cmds) > 0).
+
+deserialize_type_test() ->
+    Json = <<"\"English|0|0|0\"">>,
+    Decoded = json:decode(Json),
+    Type = auction_serialization:decode_auction_type(Decoded),
+    ?assertEqual({english, 0, 0, 0}, Type).
+
+read_add_auction_test() ->
+    Json = <<"{\"$type\":\"AddAuction\",\"at\":\"2020-05-17T08:15:16.464Z\",\"auction\":{\"id\":1,\"startsAt\":\"2018-12-01T10:00:00.000Z\",\"title\":\"Some auction\",\"expiry\":\"2020-05-18T10:00:00.000Z\",\"user\":\"BuyerOrSeller|a1|Test\",\"type\":\"English|0|0|0\",\"currency\":\"VAC\"}}">>,
+    Decoded = auction_serialization:decode(Json),
+    ?assertMatch({add_auction, _, _}, Decoded),
+    {add_auction, At, Auction} = Decoded,
+    ?assertEqual(<<"2020-05-17T08:15:16.464Z">>, At),
+    ?assertEqual(1, maps:get(id, Auction)),
+    ?assertEqual({english, 0, 0, 0}, maps:get(type, Auction)).
+
+read_place_bid_test() ->
+    Json = <<"{\"$type\":\"PlaceBid\",\"at\":\"2020-05-17T08:15:22.948Z\",\"bid\":{\"id\":\"32e692cc3fdb451da9647d6eeca5b2e3\",\"auction\":1,\"user\":\"BuyerOrSeller|a2|Buyer\",\"amount\":11,\"at\":\"2020-05-17T08:15:22.940Z\"}}">>,
+    Decoded = auction_serialization:decode(Json),
+    ?assertMatch({place_bid, _, _}, Decoded),
+    {place_bid, At, Bid} = Decoded,
+    ?assertEqual(<<"2020-05-17T08:15:22.948Z">>, At),
+    ?assertEqual(11, maps:get(amount, Bid)).
+
+write_add_auction_test() ->
+    Auction = #{id => 1,
+                starts_at => <<"2016-01-01T08:28:00.607875Z">>,
+                title => <<"auction">>,
+                expiry => <<"2016-02-01T08:28:00.607875Z">>,
+                user => #{id => <<"Sample_Seller">>, name => <<"Seller">>},
+                type => vickrey,
+                currency => <<"SEK">>},
+    Command = {add_auction, <<"2016-01-01T08:28:00.607875Z">>, Auction},
+    Encoded = auction_serialization:encode(Command),
+    %% Decode back to check equality
+    Decoded = auction_serialization:decode(iolist_to_binary(Encoded)),
+    {add_auction, _, DecodedAuction} = Decoded,
+    ?assertEqual(vickrey, maps:get(type, DecodedAuction)),
+    ?assertEqual(<<"SEK">>, maps:get(currency, DecodedAuction)).
+
+write_place_bid_test() ->
+    Bid = #{id => <<"bid_id">>,
+            auction_id => 1,
+            user => #{id => <<"Buyer_1">>, name => <<"Buyer 1">>},
+            amount => 10,
+            at => <<"2016-01-01T08:28:00.607875000001Z">>},
+    Command = {place_bid, <<"2016-02-01T07:28:00.607875Z">>, Bid},
+    Encoded = auction_serialization:encode(Command),
+    Decoded = auction_serialization:decode(iolist_to_binary(Encoded)),
+    {place_bid, _, DecodedBid} = Decoded,
+    ?assertEqual(10, maps:get(amount, DecodedBid)).
+
+parse_amount_test() ->
+    ?assertEqual({ok, {amount, <<"VAC">>, 0}}, auction_serialization:parse_amount(<<"VAC0">>)),
+    ?assertEqual({ok, {amount, <<"SEK">>, 100}}, auction_serialization:parse_amount(<<"SEK100">>)).
+
+format_amount_test() ->
+    ?assertEqual(<<"VAC0">>, auction_serialization:format_amount({amount, <<"VAC">>, 0})).
